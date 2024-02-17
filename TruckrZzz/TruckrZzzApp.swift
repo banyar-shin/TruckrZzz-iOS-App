351d890665b620dc04761435d6ff467303e3ca24
banyar-shin <banyar.minshin@gmail.com>
2024-02-17 10:20:39 -0800

    Initial Commit

---
+++ TruckrZzz/TruckrZzzApp.swift
@@ -0,0 +1,17 @@
+//
+//  TruckrZzzApp.swift
+//  TruckrZzz
+//
+//  Created by Banyar on 2/17/24.
+//
+
+import SwiftUI
+
+@main
+struct TruckrZzzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
