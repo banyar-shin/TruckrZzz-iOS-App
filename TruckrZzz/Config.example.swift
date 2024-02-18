2376bd5fad91ff4e75d10ec4947abc0b1310c937
banyar-shin <banyar.minshin@gmail.com>
2024-02-17 19:04:58 -0800

    complete RT data streaming

---
+++ TruckrZzz/Config.example.swift
@@ -0,0 +1,12 @@
+//
+//  Config.example.swift
+//  TerraRTiOSDemo
+//
+//  Created by Banyar Shin on 02/17/2024.
+//
+
+import Foundation
+
+let DEVID = "xxxxxxxxxxxxx" // your Dev ID goes here
+let XAPIKEY = "xxxxxxxxxxxxx" // your API Key goes here
+let ENDPOINT = "https://xxxxx" // your backend endpoint goes here
